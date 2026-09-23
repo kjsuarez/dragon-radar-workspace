5fc073a22e4f48d7c4aafe5cc9749829cebc9587
kjsuarez <kjsuarez94@gmail.com>
2026-09-23 01:31:20 -0400

    update dictionary

--- numpad-to-display/lib/st-7789-driver/src/dictionary.c
+++ numpad-to-display/lib/st-7789-driver/src/dictionary.c
@@ -64,24 +64,26 @@ void lookup(char character, char *output)
 {
     static const char qmark_char[] PROGMEM = {0x1c, 0x22, 0x22, 0x02, 0x04, 0x08, 0x00, 0x08};
     static const char space_char[] PROGMEM = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
-    static const char cursor_char[] PROGMEM = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,};
-    static const char zero_char[] PROGMEM =
-    static const char one_char[] PROGMEM = ;
-    static const char two_char[] PROGMEM =
-    static const char three_char[] PROGMEM =
-    static const char four_char[] PROGMEM =
-    static const char five_char[] PROGMEM =
-    static const char six_char[] PROGMEM =
-    static const char seven_char[] PROGMEM =
-    static const char eight_char[] PROGMEM =
-    static const char nine_char[] PROGMEM =
-    static const char N_char[] PROGMEM =
-    static const char S_char[] PROGMEM =
-    static const char E_char[] PROGMEM =
-    static const char W_char[] PROGMEM =
-    static const char comma_char[] PROGMEM =
-    static const char astrsc_char[] PROGMEM =
-    static const char pound_char[] PROGMEM =
+    static const char cursor_char[] PROGMEM = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
+    static const char dash_char[] PROGMEM = {0x0, 0x0, 0x0, 0x0, 0x7E, 0x0, 0x0, 0x0};
+    static const char dot_char[] PROGMEM = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x18, 0x18};
+    static const char zero_char[] PROGMEM = {0x0, 0x38, 0x44, 0x4c, 0x54, 0x64, 0x44, 0x38};
+    static const char one_char[] PROGMEM = {0x0, 0x10, 0x30, 0x10, 0x10, 0x10, 0x10, 0x38};
+    static const char two_char[] PROGMEM = {0x0, 0x38, 0x44, 0x4, 0x8, 0x10, 0x20, 0x7c};
+    static const char three_char[] PROGMEM = {0x0, 0x38, 0x44, 0x4, 0x18, 0x4, 0x44, 0x38};
+    static const char four_char[] PROGMEM = {0x0, 0x8, 0x18, 0x28, 0x48, 0x7c, 0x8, 0x8};
+    static const char five_char[] PROGMEM = {0x0, 0x7c, 0x40, 0x40, 0x78, 0x4, 0x44, 0x38};
+    static const char six_char[] PROGMEM = {0x0, 0x38, 0x44, 0x40, 0x78, 0x44, 0x44, 0x38};
+    static const char seven_char[] PROGMEM = {0x0, 0x7c, 0x4, 0x8, 0x8, 0x10, 0x10, 0x20};
+    static const char eight_char[] PROGMEM = {0x0, 0x38, 0x44, 0x44, 0x38, 0x44, 0x44, 0x38};
+    static const char nine_char[] PROGMEM = {0x0, 0x38, 0x44, 0x44, 0x3c, 0x4, 0x4, 0x38};
+    // static const char N_char[] PROGMEM =
+    // static const char S_char[] PROGMEM =
+    // static const char E_char[] PROGMEM =
+    // static const char W_char[] PROGMEM =
+    // static const char comma_char[] PROGMEM =
+    // static const char astrsc_char[] PROGMEM =
+    // static const char pound_char[] PROGMEM =
 
     // static const char A_char[] PROGMEM = {0x18, 0x24, 0x42, 0x42, 0x7E, 0x42, 0x42, 0x42};
     // static const char U_char[] PROGMEM = {0x41, 0x41, 0x41, 0x41, 0x41, 0x41, 0x22, 0x1c};
@@ -93,13 +95,40 @@ void lookup(char character, char *output)
     // static const char Y_char[] PROGMEM = {0x22, 0x22, 0x22, 0x14, 0x08, 0x08, 0x08, 0x08};
     // static const char G_char[] PROGMEM = {0x3c, 0x42, 0x42, 0x40, 0x40, 0x4e, 0x42, 0x3c};
 
-        const char *placeholder;
+    const char *placeholder;
 
     switch (character)
     {
-    // case 'A':
-    //     placeholder = A_char;
-    //     break;
+    case '0':
+        placeholder = zero_char;
+        break;
+    case '1':
+        placeholder = one_char;
+        break;
+    case '2':
+        placeholder = two_char;
+        break;
+    case '3':
+        placeholder = three_char;
+        break;
+    case '4':
+        placeholder = four_char;
+        break;
+    case '5':
+        placeholder = five_char;
+        break;
+    case '6':
+        placeholder = six_char;
+        break;
+    case '7':
+        placeholder = seven_char;
+        break;
+    case '8':
+        placeholder = eight_char;
+        break;
+    case '9':
+        placeholder = nine_char;
+        break;
     // case 'U':
     //     placeholder = U_char;
     //     break;
@@ -124,11 +153,17 @@ void lookup(char character, char *output)
     // case 'G':
     //     placeholder = G_char;
     //     break;
+    case '-':
+        placeholder = dash_char;
+        break;
     case ' ':
         placeholder = space_char;
         break;
     case '_':
         placeholder = cursor_char;
+        break;
+    case '.':
+        placeholder = dot_char;
         break;
     default:
         placeholder = qmark_char;
